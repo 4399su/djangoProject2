c6b9442fd23b6d3eb9683bc1aa5608c623cbb3d3
4399su <321396933@qq.com>
2021-01-21 13:40:42 +0800

    Cookie三天免登陆

--- thCookie/views.py
+++ thCookie/views.py
@@ -7,16 +7,26 @@ def thcookie(request):
     m = request.method
     if m == 'GET':
         if 'login' in request.COOKIES:
-            str = request.COOKIES.get("login").split(',')
+            # str = request.COOKIES.get("login").split(',')
+            str = request.get_signed_cookie('login', salt='sad').split(',')
             uname = str[0]
             pwd = str[1]
-        return render(request, "thcookie.html")
+            return render(request, "thcookie.html", {"uname": uname, "pwd": pwd})
+        else:
+            return render(request, "thcookie.html")
     else:
+        response = HttpResponse()
         uname = request.POST.get('uname')
         pwd = request.POST.get('pwd')
+        ch = request.POST.get('ch')
         if uname == 'abc' and pwd == '123':
-            response = HttpResponse("登陆成功")
-            response.set_cookie("login", uname + ',' + pwd, path='/thcookie/', max_age=24 * 60 * 60)
+            response.content = "登陆成功"
+            # response.set_cookie("login", uname + ',' + pwd, path='/thcookie/', max_age=24 * 60 * 60)
+
+            response.set_signed_cookie("login", uname + ',' + pwd, salt='sad', path='/thcookie/', max_age=24 * 60 * 60)
             return response
         else:
-            return HttpResponse("登录失败")
+            response.content = "登陆失败"
+
+            response.delete_cookie('login', path='/thcookie/')
+            return response
